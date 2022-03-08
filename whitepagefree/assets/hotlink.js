33801bdc34fa71e33ccd793144a8ef4d91b5ec3c
drmoneymaker <101156783+drmoneymaker@users.noreply.github.com>
2022-03-07 23:35:35 -0300

    Delete whitepagefree directory

--- whitepagefree/assets/hotlink.js
+++
@@ -1,6 +0,0 @@
-var getUrl = window.location;
-var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-
-if (baseUrl != "https://thisstartupdoesnotexist.com/") {
-    console.log("showads");
-}
